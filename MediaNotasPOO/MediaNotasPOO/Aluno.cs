dc7fc84d006a7caee9939eca94843a9c0e0c701b
oneto <netoweb.s@gmail.com>
2025-04-08 22:34:54 -0300

    Adiciona o exercício MediaNotasPOO

---
+++ MediaNotasPOO/MediaNotasPOO/Aluno.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaNotasPOO
+{
+    internal class Aluno
+    {
+
+        public string Nome { get; set; }
+        public int _qtdNotas;
+        private double[] _notas;
+
+        public Aluno(string name, int qtdNotas)
+        {
+            Nome = name;
+            _qtdNotas = qtdNotas;
+            _notas = new double[qtdNotas];
+        }
+
+        public double Media
+        {
+            get
+            {
+                return CalcularMedia();
+            }
+            private set { }
+        }
+
+        public void InsertNotas()
+        {
+            for (int i = 0; i < _notas.Length; i++)
+            {
+                Console.Write("Informe a " + (i + 1) + "o nota: ");
+                _notas[i] += double.Parse(Console.ReadLine());
+            }
+        }
+
+        private double CalcularMedia()
+        {
+            double total = 0;
+            for(int i=0; i< _notas.Length; i++)
+            {
+                total += _notas[i];
+            }
+
+            return total/_notas.Length;
+        }
+
+    }
+}
---
+++ MediaNotasPOO/MediaNotasPOO/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaNotasPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Informe a quantidade de alunos: ");
+            int qtdAlunos = int.Parse(Console.ReadLine());
+
+            Aluno[] aluno = new Aluno[qtdAlunos];
+            
+            for(int i = 0; i < aluno.Length; i++)
+            {
+                Console.Clear();
+                Console.Write("Informe o nome do "+(i+1) +"o aluno: ");
+                string nome = Console.ReadLine();
+                Console.Write("Informe a quantidade de notas do aluno: "+nome+": ");
+                int qtdNotas = int.Parse(Console.ReadLine());
+
+                aluno[i] = new Aluno(nome, qtdNotas);
+                aluno[i].InsertNotas();
+
+            }
+
+            foreach (Aluno a in aluno)
+            {
+                Console.WriteLine("Aluno: " + a.Nome);
+                Console.WriteLine("Média: " + a.Media);
+            }
+
+        }
+    }
+}
